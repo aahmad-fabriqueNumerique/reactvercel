e4ba42f6d2fdc4dd5a002a6fb06d719e9cb9b220
aahmad-fabriquenumerique <aahmad@fabriquenumerique.fr>
2024-07-04 15:33:34 +0200

    modification app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+  const [voiture, setVoiture] = useState([]);
+  useEffect(() => {
+    const url = process.env.VERCELAPIURL
+     fetch=(`${url}/list`)
+        .then((response) => response.json())
+        .then((data) => {
+           console.log(data);
+           setVoiture(data);
+        })
+        .catch((err) => {
+           console.log(err.message);
+        });
+  }, []);
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Je veux pas apprendre REACT
-        </a>
-      </header>
-    </div>
+    
+    <div className="posts-container">
+      <h1>testeststestestestes</h1>
+    {voiture.map((voiture) => {
+       return (
+          <div className="post-card" key={voiture.make}>
+             <h2 className="post-title">{voiture.model}</h2>
+             <p className="post-body">{voiture.price}</p>
+      
+          </div>
+       );
+    })}
+ </div>
   );
 }
 
