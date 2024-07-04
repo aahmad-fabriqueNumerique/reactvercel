a23ed33ec70b9bf9a09c322293389ab24df0f551
aahmad-fabriquenumerique <aahmad@fabriquenumerique.fr>
2024-07-04 14:52:22 +0200

    modif app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Je veux pas apprendre REACT
         </a>
       </header>
     </div>
